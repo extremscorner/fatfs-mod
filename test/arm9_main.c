238f0b47116ee17bce6f8cfc9890b5e1db191158
fincs <fincs@devkitpro.org>
2023-03-25 14:13:19 +0100

    Add simple FatFs tester program

---
+++ test/arm9_main.c
@@ -0,0 +1,146 @@
+#include <calico.h>
+#include <ff.h>
+#include <diskio.h>
+
+#include <stdlib.h>
+#include <string.h>
+
+void consoleInit(void);
+
+void systemUserStartup(void)
+{
+	// Enable graphics
+	MEOW_REG(u32, IO_GFX_A + IO_DISPCNT) = 1U<<16;
+	MEOW_REG(u32, IO_GFX_B + IO_DISPCNT) = 1U<<16;
+	threadWaitForVBlank();
+}
+
+static Mutex s_fatMutex;
+
+int ff_mutex_create(int vol)
+{
+	dietPrint("ff_mutex_create(%d)\n", vol);
+	return 1;
+}
+
+void ff_mutex_delete(int vol)
+{
+	dietPrint("ff_mutex_delete(%d)\n", vol);
+}
+
+int ff_mutex_take(int vol)
+{
+	//dietPrint("ff_mutex_take(%d)\n", vol);
+	mutexLock(&s_fatMutex);
+	return 1;
+}
+
+void ff_mutex_give(int vol)
+{
+	//dietPrint("ff_mutex_give(%d)\n", vol);
+	mutexUnlock(&s_fatMutex);
+}
+
+static BlkDevice s_curDev;
+
+DSTATUS disk_initialize(BYTE pdrv)
+{
+	dietPrint("disk_initialize(%u)\n", pdrv);
+	if (blkDevInit(BlkDevice_Dldi)) {
+		s_curDev = BlkDevice_Dldi;
+	} else if (blkDevInit(BlkDevice_TwlSdCard)) {
+		s_curDev = BlkDevice_TwlSdCard;
+	} else {
+		return RES_ERROR;
+	}
+	return RES_OK;
+}
+
+DSTATUS disk_status(BYTE pdrv)
+{
+	//dietPrint("disk_status(%u)\n", pdrv);
+	return RES_OK;
+}
+
+#define DISK_BUF_NUM_SECTORS 16
+alignas(ARM_CACHE_LINE_SZ) static u8 s_diskBuf[DISK_BUF_NUM_SECTORS*512];
+
+DRESULT disk_read(BYTE pdrv, BYTE* buff, LBA_t sector, UINT count)
+{
+	dietPrint("RD%u %p 0x%lx %u\n", pdrv, buff, sector, count);
+
+	while (count) {
+		UINT this_count = count > DISK_BUF_NUM_SECTORS ? DISK_BUF_NUM_SECTORS : count;
+		if (!blkDevReadSectors(s_curDev, s_diskBuf, sector, this_count)) {
+			return RES_ERROR;
+		}
+
+		memcpy(buff, s_diskBuf, this_count*512);
+		buff += this_count*512;
+
+		sector += this_count;
+		count -= this_count;
+	}
+
+	return RES_OK;
+}
+
+DRESULT disk_write(BYTE pdrv, const BYTE* buff, LBA_t sector, UINT count)
+{
+	dietPrint("WR%u %p 0x%lx %u\n", pdrv, buff, sector, count);
+	return RES_WRPRT;
+}
+
+int main(int argc, char* argv[])
+{
+	consoleInit();
+	blkInit();
+
+	dietPrint("Hello, world!\n");
+
+	static FATFS fs;
+	FRESULT res;
+	res = f_mount(&fs, "0:/", 1);
+	if (res != FR_OK) {
+		dietPrint("f_mount returned %d\n", res);
+	} else {
+		dietPrint("Mount OK\n");
+
+		FFDIR dir;
+		res = f_opendir(&dir, "0:/");
+		if (res == FR_OK) {
+
+			for (;;) {
+				static FILINFO info;
+				res = f_readdir(&dir, &info);
+				if (res != FR_OK || !info.fname[0]) {
+					break;
+				}
+
+				if (info.fattrib & AM_DIR) {
+					dietPrint("[%s]\n", info.fname);
+				} else {
+					dietPrint(" %s\n", info.fname);
+				}
+			}
+
+			f_closedir(&dir);
+		}
+	}
+
+	dietPrint("Push START to exit\n");
+
+	Keypad pad;
+	keypadRead(&pad);
+
+	while (pmMainLoop()) {
+		threadWaitForVBlank();
+		keypadRead(&pad);
+
+		if (keypadDown(&pad) & KEY_START) {
+			break;
+		}
+	}
+
+	return 0;
+}
---
+++ test/console.c
@@ -0,0 +1,100 @@
+#include <calico.h>
+
+#include "font_bin.h"
+
+#define PALRAM ((vu16*)MM_PALRAM)
+
+static Mutex s_conMutex;
+static u16* s_conTilemap;
+static int s_conCursorX, s_conCursorY;
+
+static void consoleNewRow(void)
+{
+	s_conCursorY ++;
+	if (s_conCursorY >= 24) {
+		s_conCursorY --;
+		svcCpuSet(&s_conTilemap[32], &s_conTilemap[0], SVC_SET_UNIT_16 | SVC_SET_SIZE_16(23*32*2));
+	}
+	u16 fill = 0;
+	svcCpuSet(&fill, &s_conTilemap[s_conCursorY*32], SVC_SET_UNIT_16 | SVC_SET_FIXED | SVC_SET_SIZE_16(32*2));
+}
+
+static void consolePutChar(int c)
+{
+	if (s_conCursorX >= 32 && c != '\n') {
+		s_conCursorX = 0;
+		consoleNewRow();
+	}
+
+	switch (c) {
+		default:
+			s_conTilemap[s_conCursorY*32 + s_conCursorX] = c;
+			s_conCursorX++;
+			break;
+
+		case '\t':
+			s_conCursorX = (s_conCursorX + 4) &~ 3;
+			break;
+
+		case '\n':
+			consoleNewRow();
+			/* fallthrough */
+
+		case '\r':
+			s_conCursorX = 0;
+			break;
+	}
+}
+
+static void consoleWrite(const char* buf, size_t size)
+{
+	mutexLock(&s_conMutex);
+
+	if_likely (buf) {
+		for (size_t i = 0; i < size; i ++) {
+			consolePutChar(buf[i]);
+		}
+	} else {
+		for (size_t i = 0; i < size; i ++) {
+			consolePutChar(' ');
+		}
+	}
+
+	mutexUnlock(&s_conMutex);
+}
+
+void consoleInit(void)
+{
+	REG_VRAMCNT_A = VRAM_CONFIG(1, 0);
+
+	SvcBitUnpackParams params = {
+		.in_length_bytes = font_bin_size,
+		.in_width_bits   = 1,
+		.out_width_bits  = 4,
+		.data_offset     = 14, // 1+14 = 15
+		.zero_data_flag  = 0,
+	};
+	svcBitUnpack(font_bin, (void*)MM_VRAM_BG_A, &params);
+
+	MEOW_REG(u16, IO_GFX_A + IO_BGxCNT(0)) = 0 | (0<<2) | (0<<7) | (4<<8) | (0<<14);
+	MEOW_REG(u32, IO_GFX_A + IO_DISPCNT) |= (1<<8);
+
+	s_conTilemap = (u16*)(MM_VRAM_BG_A + 8*1024);
+
+	PALRAM[0] = 0 | (0<<5) | (0<<10);
+	PALRAM[15] = 24 | (24<<5) | (24<<10);
+	PALRAM[0x200] = 0 | (0<<5) | (0<<10);
+
+	dietPrintSetFunc(consoleWrite);
+	installArm7DebugSupport(consoleWrite, MAIN_THREAD_PRIO-1);
+}
+
+void consoleHorridHack(unsigned pos)
+{
+	mutexLock(&s_conMutex);
+	if (s_conCursorY >= 0) {
+		s_conCursorY --;
+	}
+	s_conCursorX = pos;
+	mutexUnlock(&s_conMutex);
+}
