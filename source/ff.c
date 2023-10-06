extremscorner/fatfs-mod

commit dc0a006cccb225468455b3ae8e6baef0e4e38f36

Don't hide dotfiles in directory listings...

source/ff.c
@@ -2319,7 +2319,7 @@ static FRESULT dir_read (
 		{	/* On the FAT/FAT32 volume */
 			dp->obj.attr = attr = dp->dir[DIR_Attr] & AM_MASK;	/* Get attribute */
 #if FF_USE_LFN		/* LFN configuration */
-			if (b == DDEM || b == '.' || (int)((attr & ~AM_ARC) == AM_VOL) != vol) {	/* An entry without valid data */
+			if (b == DDEM || /*b == '.' ||*/ (int)((attr & ~AM_ARC) == AM_VOL) != vol) {	/* An entry without valid data */
 				ord = 0xFF;
 			} else {
 				if (attr == AM_LFN) {	/* An LFN entry is found */
@@ -2338,7 +2338,7 @@ static FRESULT dir_read (
 				}
 			}
 #else		/* Non LFN configuration */
-			if (b != DDEM && b != '.' && attr != AM_LFN && (int)((attr & ~AM_ARC) == AM_VOL) == vol) {	/* Is it a valid entry? */
+			if (b != DDEM && /*b != '.' &&*/ attr != AM_LFN && (int)((attr & ~AM_ARC) == AM_VOL) == vol) {	/* Is it a valid entry? */
 				break;
 			}
 #endif

commit c2e434226ff74cbd6241ab6fbf03e2c701e81df1

Fix directory deletion (broken by dc0a006c)

source/ff.c
@@ -4889,8 +4889,17 @@ FRESULT f_unlink (
 #endif
 						res = dir_sdi(&sdj, 0);
 						if (res == FR_OK) {
-							res = DIR_READ_FILE(&sdj);			/* Test if the directory is empty */
-							if (res == FR_OK) res = FR_DENIED;	/* Not empty? */
+							/* Test if the directory is empty */
+							do {
+								res = DIR_READ_FILE(&sdj);
+								if (res == FR_OK) {
+									if (sdj.dir[DIR_Name] == '.') {
+										res = dir_next(&sdj, 0);		/* Skip over dot entries */
+									} else {
+										res = FR_DENIED;	/* Not empty? */
+									}
+								}
+							} while (res == FR_OK);
 							if (res == FR_NO_FILE) res = FR_OK;	/* Empty? */
 						}
 					}